64d149925d80d7c5854a0a8fdc47dc2662d30944
hoseinsedaqat <66621982+hoseinsedaqat@users.noreply.github.com>
2021-03-08 16:34:37 +0330

    Add files via upload

---
+++ precache-manifest.264b68af87c3cb94441be40c0128986d.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef222e899d65e9bc946",
+    "url": "/css/app.5b0c79b5.css"
+  },
+  {
+    "revision": "03bf1002403daac58996",
+    "url": "/css/chunk-vendors.6acb310d.css"
+  },
+  {
+    "revision": "a34dc5a98b5e696a36606d11d15db495",
+    "url": "/img/note_icons/android-icon-192x192-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "d27204142d54c36767a1fee78849266e",
+    "url": "/img/note_icons/apple-icon-114x114-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "9b09156c0a2e1b38c9bda9e68c477a2b",
+    "url": "/img/note_icons/apple-icon-120x120-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "08d4b5c6302e8f3299e049070ee8e507",
+    "url": "/img/note_icons/apple-icon-144x144-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "9e634a686a865ccb0b0569902d4f9188",
+    "url": "/img/note_icons/apple-icon-152x152-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "e02cbb9265e80205dd0504276810755d",
+    "url": "/img/note_icons/apple-icon-180x180-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "f24cec9b02ca951c11e3207402c49295",
+    "url": "/img/note_icons/apple-icon-57x57-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "e504a70b181745ded997976d97af7853",
+    "url": "/img/note_icons/apple-icon-60x60-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "c321be0b1e33637f2960eb9111a60c45",
+    "url": "/img/note_icons/apple-icon-72x72-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "9609c4126aace9c68f4356e2a7a01656",
+    "url": "/img/note_icons/apple-icon-76x76-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "07ee6e4b67cb25fe45926955f865c795",
+    "url": "/img/note_icons/favicon-16x16-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "226328488455ec8fa3b7bd9641138f12",
+    "url": "/img/note_icons/favicon-32x32-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "8b2d720a022a34e729ae968e3e9d39fc",
+    "url": "/img/note_icons/favicon-96x96-dunplab-manifest-37607.png"
+  },
+  {
+    "revision": "a9efcccd55e516b2656c89a2ed0e7ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ef222e899d65e9bc946",
+    "url": "/js/app.78d7ac8d.js"
+  },
+  {
+    "revision": "03bf1002403daac58996",
+    "url": "/js/chunk-vendors.d9e809d2.js"
+  },
+  {
+    "revision": "63b487456d2899204d20df4b44ec754f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
